e28fe3bc7d4123bb65962c617c38d7b5cfc46f95
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-10-11 17:22:24 -0300

    Formated files

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -13,27 +13,24 @@ extern uint8_t endOfKernel;
 
 static const uint64_t PageSize = 0x1000;
 
-static void * const sampleCodeModuleAddress = (void*)0x400000;
-static void * const sampleDataModuleAddress = (void*)0x500000;
+static void *const sampleCodeModuleAddress = (void *)0x400000;
+static void *const sampleDataModuleAddress = (void *)0x500000;
 
 typedef int (*EntryPoint)();
 
-
-void clearBSS(void * bssAddress, uint64_t bssSize)
+void clearBSS(void *bssAddress, uint64_t bssSize)
 {
 	memset(bssAddress, 0, bssSize);
 }
 
-void * getStackBase()
+void *getStackBase()
 {
-	return (void*)(
-		(uint64_t)&endOfKernel
-		+ PageSize * 8				//The size of the stack itself, 32KiB
-		- sizeof(uint64_t)			//Begin at the top of the stack
+	return (void *)((uint64_t)&endOfKernel + PageSize * 8 // The size of the stack itself, 32KiB
+					- sizeof(uint64_t)					  // Begin at the top of the stack
 	);
 }
 
-void * initializeKernelBinary()
+void *initializeKernelBinary()
 {
 	char buffer[10];
 
@@ -46,10 +43,9 @@ void * initializeKernelBinary()
 
 	ncPrint("[Loading modules]");
 	ncNewline();
-	void * moduleAddresses[] = {
+	void *moduleAddresses[] = {
 		sampleCodeModuleAddress,
-		sampleDataModuleAddress
-	};
+		sampleDataModuleAddress};
 
 	loadModules(&endOfKernelBinary, moduleAddresses);
 	ncPrint("[Done]");
@@ -81,7 +77,7 @@ void * initializeKernelBinary()
 }
 
 int main()
-{	
+{
 	ncPrint("[Kernel Main]");
 	ncNewline();
 	ncPrint("  Sample code module at 0x");
@@ -96,7 +92,7 @@ int main()
 	ncPrintHex((uint64_t)sampleDataModuleAddress);
 	ncNewline();
 	ncPrint("  Sample data module contents: ");
-	ncPrint((char*)sampleDataModuleAddress);
+	ncPrint((char *)sampleDataModuleAddress);
 	ncNewline();
 
 	ncPrint("[Finished]");

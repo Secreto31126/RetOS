a961d92ff4d819d92961032ae7559deead20c3a3
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-10-13 16:04:07 -0300

    Kinda working debug mode

--- run.sh
+++ run.sh
@@ -8,14 +8,26 @@ then
     exit 1
 fi
 
-id=$(docker run -d -v "/$path/Image:/root/Image" "$container")
+id=
+if [ "$1" = "DEBUG" ]
+then
+    echo "Debug mode"
+    id=$(docker run -d -v "/$path/Image:/root/Image" "$container" all EGCCFLAGS="-g")
+else
+    id=$(docker run -d -v "/$path/Image:/root/Image" "$container" all)
+fi
 docker wait "$id"
 
 clear
 docker logs "$id"
 docker rm "$id" > /dev/null
 
-qemu-system-x86_64 -hda "$path/Image/x64BareBonesImage.qcow2" -m 512
-
-make clean -CToolchain > /dev/null
-make clean > /dev/null
+if [ "$1" = "DEBUG" ]
+then
+    echo "Debug mode"
+    # qemu-system-x86_64 -s -S -hda "$path/Image/x64BareBonesImage.qcow2" -m 512 &
+else
+    qemu-system-x86_64 -hda "$path/Image/x64BareBonesImage.qcow2" -m 512
+    make clean -CToolchain > /dev/null
+    make clean > /dev/null
+fi

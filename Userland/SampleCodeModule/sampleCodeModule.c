8dba8dfd9ad3695ca23a62cc0b5d860637a67d32
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-10-14 19:29:26 -0300

    DEADC0DE

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -12,26 +12,6 @@ int strlen(char *s)
 	return i;
 }
 
-char *ulltoa(unsigned long long ll, char *buffer, int radix)
-{
-	while (ll > radix)
-	{
-		*buffer = (ll % radix) + '0';
-		if (*buffer > '9')
-			*buffer += 'A' - '9' - 1;
-		buffer++;
-		ll /= radix;
-	}
-
-	*buffer = ll + '0';
-	if (*buffer > '9')
-		*buffer += 'A' - '9' - 1;
-
-	buffer++;
-	*buffer = 0;
-	return buffer;
-}
-
 int main()
 {
 	char buffer[100];

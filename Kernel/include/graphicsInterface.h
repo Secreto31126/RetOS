93e986fa4bab2ec0407bf0cbe4640b6493bd3a22
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2023-10-17 14:18:06 -0300

    Basic function declarations

---
+++ Kernel/include/graphicsInterface.h
@@ -0,0 +1,7 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdint.h>
+void drawFromArray(uint64_t *array, uint64_t lineLength, uint64_t lineCount);
+
+#endif
---
+++ Kernel/vidlib/graphicsInterface.c
@@ -0,0 +1,2 @@
+#include <graphicsInterface.h>
+void drawFromArray(uint64_t *array, uint64_t lineLength, uint64_t);

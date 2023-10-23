5d0782a43ddc1ffb18251cc037126f507162282a
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2023-10-23 13:25:18 -0300

    use pointer

--- Userland/SampleCodeModule/window/fontInterface.c
+++ Userland/SampleCodeModule/window/fontInterface.c
@@ -3,14 +3,14 @@
 #include "./../nstdlib/nstdlib.h"
 #define FONT_COUNT 1
 #define ASCII_MAX 128
-typedef struct font
+typedef struct
 {
     char *name;
     char *letters;
 } font;
 
 static font fonts[FONT_COUNT];
-static font currentFont;
+static font *currentFont;
 void loadFont(char *fontName, char *fontLetters, int fontNumber)
 {
     fonts[fontNumber % FONT_COUNT].name = fontName;
@@ -18,10 +18,11 @@ void loadFont(char *fontName, char *fontLetters, int fontNumber)
 }
 void setFont(int fontNumber)
 {
-    currentFont = fonts[fontNumber % FONT_COUNT];
+    currentFont = fonts + (fontNumber % FONT_COUNT);
 }
 void initialize()
 {
+    return;
     loadFont("Default", Classic, 0);
     setFont(0);
 }
@@ -40,7 +41,7 @@ char drawStringToWindow(Window w, char *string, HexColor letterColor, HexColor h
             if (string[index])
             {
                 char *letter;
-                *letter = currentFont.letters[string[index] % ASCII_MAX]; // Surprisingly enough, this is correct
+                *letter = currentFont->letters[string[index] % ASCII_MAX]; // Surprisingly enough, this is correct
                 HexColor aux[TRUE_LETTER_HEIGHT][TRUE_LETTER_WIDTH];
                 drawFromHexArray(w, toHexArray(letter, aux, TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, 2, highlightColor, letterColor), TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, (int)(j + 0.5), (int)(i + 0.5), size, size); // Arte.
                 index++;

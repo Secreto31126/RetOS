1ad40161e1ce5d268d2c4c7aa12fd6ed59a31292
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-01 15:05:22 -0300

    Update free.c

--- Kernel/memory/free.c
+++ Kernel/memory/free.c
@@ -2,7 +2,7 @@
 
 void free(void *ptr)
 {
-    map_entry *pointer = (map_entry *)((uint8_t *)ptr - heap_size);
+    map_entry *pointer = (map_entry *)((uint64_t)ptr - heap_size);
     if (map_start <= pointer && pointer < map_start + map_size)
     {
         *pointer = 0;

59d4833f4010585d1f11ece73f2abcc3285bb64c
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-02 22:40:32 -0300

    Improved current code quality

--- Kernel/video/driver.c
+++ Kernel/video/driver.c
@@ -1,20 +1,13 @@
 #include "video.h"
 
 VBEInfo *VBE_mode_info = (VBEInfo *)0x5C00; // hardcoded
+#define FRAMEBUFFER ((framebuffer_element *)((uint64_t)VBE_mode_info->framebuffer))
 
 #define GET_HEX(a, r, g, b) (((a) << 24) + ((r) << 16) + ((g) << 8) + (b))
 #define GET_OPACITY(x) (((x) >> 24) & 0xFF)
 #define GET_RED(x) (((x) >> 16) & 0xFF)
 #define GET_GREEN(x) (((x) >> 8) & 0xFF)
 #define GET_BLUE(x) ((x) & 0xFF)
-
-/**
- * @brief Only needed for the sizeof :)
- */
-typedef struct
-{
-    char i, d, k;
-} framebuffer_elements;
 
 int putPixel(HexColor hexColor, uint64_t x, uint64_t y)
 {
@@ -23,22 +16,27 @@ int putPixel(HexColor hexColor, uint64_t x, uint64_t y)
 
 int putPixelStd(uint8_t opacity, uint8_t red, uint8_t green, uint8_t blue, uint64_t x, uint64_t y)
 {
+    // Gotta go fast
+    if (!opacity)
+        return 1;
+
     if (x >= VBE_mode_info->width || y >= VBE_mode_info->height)
         return 0;
 
-    // Gotta go fast
-    if (!opacity)
-    {
-        return 1;
-    }
+    if (!VBE_mode_info->bpp)
+        return 0;
 
-    uint8_t *framebuffer = (uint8_t *)VBE_mode_info->framebuffer; // tira warning. Pasas que pasan
-    // En el array, cada pixel tiene tres valores (RGB), por lo que la posición en el array es x*3
-    uint64_t offset = x * (VBE_mode_info->bpp / 8) + y * VBE_mode_info->pitch; // es 3 lo primero, la cantidad de líneas (y) por el tamaño de una línea lo segundo
-    framebuffer[offset] = mergeColor(framebuffer[offset], blue, opacity);
-    framebuffer[offset + 1] = mergeColor(framebuffer[offset + 1], green, opacity);
-    framebuffer[offset + 2] = mergeColor(framebuffer[offset + 2], red, opacity);
+    framebuffer_element *framebuffer = FRAMEBUFFER;
 
+    uint64_t offset = x + y * VBE_mode_info->pitch / (VBE_mode_info->bpp / 8);
+    framebuffer_element old_pixel = framebuffer[offset];
+    framebuffer_element new_pixel = {
+        .b = mergeColor(old_pixel.b, blue, opacity),
+        .g = mergeColor(old_pixel.g, green, opacity),
+        .r = mergeColor(old_pixel.r, red, opacity),
+    };
+
+    framebuffer[offset] = new_pixel;
     return 1;
 }
 
@@ -76,7 +74,7 @@ uint64_t drawFromArray(HexColor *array, uint32_t width, uint32_t height, uint32_
 void clear_screen()
 {
     uint64_t size = VBE_mode_info->width * VBE_mode_info->height;
-    uint64_t *end = (uint64_t *)((framebuffer_elements *)VBE_mode_info->framebuffer + size);
+    uint64_t *end = (uint64_t *)(FRAMEBUFFER + size);
 
     uint64_t *i = VBE_mode_info->framebuffer;
     while (i < end - 1)

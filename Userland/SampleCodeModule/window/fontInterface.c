6e2ab26a50da8f9abefcb46616fad1cf519c5016
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-07 14:44:49 -0300

    Incompatible pointer type warning

--- Userland/SampleCodeModule/window/fontInterface.c
+++ Userland/SampleCodeModule/window/fontInterface.c
@@ -22,7 +22,7 @@ void setFont(int fontNumber)
 }
 void initializeFonts()
 {
-    loadFont("Default", Classic, 0);
+    loadFont("Default", (char *)Classic, 0);
     setFont(0);
 }
 /**
@@ -43,7 +43,7 @@ char drawStringToWindow(Window w, char *string, HexColor letterColor, HexColor h
                 char *letter;
                 letter = (currentFont->letters) + ((int)string[index] % ASCII_MAX) * TRUE_LETTER_HEIGHT * TRUE_LETTER_WIDTH; // Surprisingly enough, this is correct
                 HexColor aux[TRUE_LETTER_HEIGHT][TRUE_LETTER_WIDTH];
-                drawFromHexArray(w, toHexArray(letter, aux, TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, 2, highlightColor, letterColor), TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, (int)(j + 0.5), (int)(i + 0.5), size, size); // Arte.
+                drawFromHexArray(w, toHexArray(letter, (HexColor *)aux, TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, 2, highlightColor, letterColor), TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, (int)(j + 0.5), (int)(i + 0.5), size, size); // Arte.
                 index++;
             }
             else

43a8bd05b20e5d0a726593e23bcbc25e0c84df54
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-06 23:32:20 -0300

    Small utility before big code

--- Kernel/lib/lib.c
+++ Kernel/lib/lib.c
@@ -49,6 +49,21 @@ void *memcpy(void *destination, const void *source, uint64_t length)
 	return destination;
 }
 
+int strcmp(const char *p1, const char *p2)
+{
+	const unsigned char *s1 = (const unsigned char *)p1;
+	const unsigned char *s2 = (const unsigned char *)p2;
+	unsigned char c1, c2;
+	do
+	{
+		c1 = (unsigned char)*s1++;
+		c2 = (unsigned char)*s2++;
+		if (c1 == '\0')
+			return c1 - c2;
+	} while (c1 == c2);
+	return c1 - c2;
+}
+
 void dump_regs_hex_magician(char *s, uint8_t r)
 {
 	char hextable[] = "0123456789ABCDEF";

856a502813a58ca0d55f056d6e4cd33851e74b80
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2023-11-07 16:07:57 -0300

    Array subscript has type char

--- Userland/SampleCodeModule/piano/piano.c
+++ Userland/SampleCodeModule/piano/piano.c
@@ -77,7 +77,7 @@ void startPiano()
         default:
             if (note > '0' && note <= '6')
             {
-                char index = note - '1';
+                unsigned char index = note - '1';
                 paintString(songs[index].name, -1, 0);
                 songs[index].song();
             }

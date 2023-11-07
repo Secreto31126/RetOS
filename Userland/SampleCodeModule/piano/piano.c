cab9a131a9d36b6b9a0a391a9cc5bf8e84ecf754
Tobias-Noceda <tnoceda@itba.edu.ar>
2023-11-07 11:41:32 -0300

    Final piano

--- Userland/SampleCodeModule/piano/piano.c
+++ Userland/SampleCodeModule/piano/piano.c
@@ -18,77 +18,78 @@ void startPiano()
     blank();
     paintString("Welcome to the piano!\n\nPress 'q' to exit.\n", -1, 0);
     char note;
-    while ((note = getChar()) != 'q')
+    while ((note = readChar()) != 'q')
     {
         switch (note)
         {
         case 'z':
             play(261);
-            paintString("Do\n", -1, 0);
+            // paintString("Do\n", -1, 0);
             break;
         case 's':
             play(277);
-            paintString("Do#\n", -1, 0);
+            // paintString("Do#\n", -1, 0);
             break;
         case 'x':
             play(293);
-            paintString("Re\n", -1, 0);
+            // paintString("Re\n", -1, 0);
             break;
         case 'd':
             play(311);
-            paintString("Re#\n", -1, 0);
+            // paintString("Re#\n", -1, 0);
             break;
         case 'c':
             play(329);
-            paintString("Mi\n", -1, 0);
+            // paintString("Mi\n", -1, 0);
             break;
         case 'v':
             play(349);
-            paintString("Fa\n", -1, 0);
+            // paintString("Fa\n", -1, 0);
             break;
         case 'g':
             play(369);
-            paintString("Fa#\n", -1, 0);
+            // paintString("Fa#\n", -1, 0);
             break;
         case 'b':
             play(392);
-            paintString("Sol\n", -1, 0);
+            // paintString("Sol\n", -1, 0);
             break;
         case 'h':
             play(415);
-            paintString("Sol#\n", -1, 0);
+            // paintString("Sol#\n", -1, 0);
             break;
         case 'n':
             play(440);
-            paintString("La\n", -1, 0);
+            // paintString("La\n", -1, 0);
             break;
         case 'j':
             play(466);
-            paintString("La#\n", -1, 0);
+            // paintString("La#\n", -1, 0);
             break;
         case 'm':
             play(493);
-            paintString("Si\n", -1, 0);
+            // paintString("Si\n", -1, 0);
             break;
         case ',':
             play(523);
-            paintString("Do\n", -1, 0);
+            // paintString("Do\n", -1, 0);
             break;
         default:
             if (note > '0' && note <= '6')
             {
                 char index = note - '1';
-                songs[index].song();
                 paintString(songs[index].name, -1, 0);
+                songs[index].song();
             }
             shut();
             break;
         }
-        while ((note == readChar()) && (note != 0))
-        {
+
+        char tmp = note;
+        while (tmp == (note = readChar()) && note != 0)
             halt_user();
-        }
-        shut();
+
+        halt_user();
     }
     free(songs);
     blank();
@@ -97,15 +98,15 @@ void startPiano()
 void addSongs()
 {
     songs[0].song = imperial_march;
-    songs[0].name = "Imperial March";
+    songs[0].name = "Imperial March\n";
     songs[1].song = hes_a_pirate;
-    songs[1].name = "He's a Pirate";
+    songs[1].name = "He's a Pirate\n";
     songs[2].song = outer_wilds;
-    songs[2].name = "Outer Wilds";
+    songs[2].name = "Outer Wilds\n";
     songs[3].song = do_i_wanna_know;
-    songs[3].name = "Do I Wanna Know";
+    songs[3].name = "Do I Wanna Know\n";
     songs[4].song = sports_center;
-    songs[4].name = "Sports Center";
+    songs[4].name = "Sports Center\n";
     songs[5].song = here_comes_the_sun;
-    songs[5].name = "Here Comes the Sun";
+    songs[5].name = "Here Comes the Sun\n";
 }

291b73167031dc313044f6e1f371c94c1eddf34c
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-03-07 23:15:12 -0300

    Testing...

--- Kernel/loader.asm
+++ Kernel/loader.asm
@@ -7,13 +7,17 @@
 userland	db "module", 0
 tomyland	db "tomyland", 0
 
+hello		db "Hello", 0
+world		db "World", 0
+argv		dq hello, world, 0
+
 	section .text
 
 loader:
 	call	initializeKernelBinary	; Set up the kernel binary
 	; mov		rdi, userland
 	mov		rdi, tomyland
-	mov		rsi, 0
+	mov		rsi, argv
 	call 	execv
 .hang:
 	cli

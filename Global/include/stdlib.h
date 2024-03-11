ad115b8b9f90fa3e8e0cbd32b6abcbed7a793b4f
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-03-11 13:20:55 -0300

    Even more cool things

---
+++ Global/include/stdlib.h
@@ -0,0 +1,9 @@
+#ifndef GLIB_H
+#define GLIB_H
+
+#include <stddef.h>
+
+void *memset(void *destination, int character, size_t length);
+void *memcpy(void *destination, const void *source, size_t length);
+
+#endif

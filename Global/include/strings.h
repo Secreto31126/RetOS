9ac52601ce39f871d021029db8890205389564b0
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-03-11 12:31:54 -0300

    Small example

---
+++ Global/include/strings.h
@@ -0,0 +1,14 @@
+#ifndef STR_H
+#define STR_H
+
+#include <stddef.h>
+
+/**
+ * @brief Get the length of a string
+ *
+ * @param str The string
+ * @return size_t The length of the string
+ */
+size_t strlen(const char *str);
+
+#endif

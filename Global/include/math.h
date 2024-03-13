2bcd7b7aabee7526e09716a8ccbc2750acf1c9b3
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-03-13 20:35:02 -0300

    Added Doxygen

--- Global/include/math.h
+++ Global/include/math.h
@@ -3,6 +3,15 @@
 
 #define PI 3.14159265359
 
+/**
+ * @brief Exponential function
+ *
+ * @note can't return doubles in userland because SSE registers aren't usable
+ *
+ * @param x Base
+ * @param y Exponent
+ * @return unsigned long long Result
+ */
 unsigned long long pow(double x, int y);
 
 #endif
--- Global/math/pow.c
+++ Global/math/pow.c
@@ -2,8 +2,8 @@
 
 unsigned long long pow(double x, int y)
 {
-    int ans = 1.0;
-    for (int i = 0; i < y; i++)
+    unsigned long long ans = 1.0;
+    for (unsigned int i = 0; i < y; i++)
         ans *= x;
     return ans;
 }

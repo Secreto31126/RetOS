4b6218080abe7270d8d1437ee1c05cbf5eb72b67
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-03-18 12:42:19 -0300

    should (might) be working now

--- Userland/SampleCodeModule/textInterface/textBlock.c
+++ Userland/SampleCodeModule/textInterface/textBlock.c
@@ -1,6 +1,10 @@
 #include "textBlock.h"
 #include "./../window/fonts.h"
 
+static HexColor *stampPixels[TRUE_LETTER_HEIGHT * TRUE_LETTER_WIDTH];
+void drawCharAt(char c, HexColor letterColor, HexColor highlightColor, uint64_t x, uint64_t y);
+void drawTextBlock(textBlock t);
+
 textBlock getTextBlock(uint64_t width, uint64_t height)
 {
     textBlock toRet = malloc(sizeof(textBlockHeader));
@@ -40,28 +44,51 @@ void putChar(textBlock t, char c)
     if (!willCauseLineBreak(t, c))
     {
         addChar(last, c);
-        // paintCharAt
+        drawCharAt(c, t->textColor, t->highlightColor, last->size * TRUE_LETTER_WIDTH, t->lines->size * TRUE_LETTER_HEIGHT);
     }
     else
     {
         if (noSpaceOnBottom(t))
         {
             remove(t->lines, 0);
-            // rePaintAll
+            clear();
+            drawTextBlock(t);
         }
         string newLine = createString();
         addChar(newLine, c);
         add(t->lines, newLine);
         if (c != '\n')
-        {
-            // paintCharAt
-        }
+            drawCharAt(c, t->textColor, t->highlightColor, last->size * TRUE_LETTER_WIDTH, t->lines->size * TRUE_LETTER_HEIGHT);
     }
+}
+
+void drawTextBlock(textBlock t)
+{
+    listIterator lIter = getListIterator(t->lines);
+    int x = 0, y = 0;
+    while (hasNextL(lIter))
+    {
+        stringIterator sIter = next(lIter);
+        while (hasNextS(sIter))
+        {
+            drawCharAt(nextS(sIter), t->textColor, t->highlightColor, x, y);
+            x += TRUE_LETTER_WIDTH;
+        }
+        y += TRUE_LETTER_HEIGHT;
+        free(sIter);
+    }
+    free(lIter); // The true garbage collector
+}
+
+void drawCharAt(char c, HexColor letterColor, HexColor highlightColor, uint64_t x, uint64_t y)
+{
+    Window stamp = getWindow(TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, stampPixels);
+    drawCharToWindow(stamp, c, letterColor, highlightColor);
+    drawWindow(stamp, x, y);
 }
 
 void putString(textBlock t, char *s)
 {
     while (*s)
-    {
-    }
+        putChar(t, *s++);
 }

12135d11c80596a9e0640bad597f050af1c52003
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-04-29 18:58:21 -0300

    Ups

--- Kernel/asm/exec.asm
+++ Kernel/asm/exec.asm
@@ -3,7 +3,7 @@
 ; void portal_to_userland(int (*code)(void), uint64_t rsp);
 portal_to_userland:
 	push qword  0		; SS
-	push		rdi		; RSP
+	push		rsi		; RSP
     push qword	0x202	; RFLAGS
 	push qword	0x8		; CS
 	push		rdi		; Userland code

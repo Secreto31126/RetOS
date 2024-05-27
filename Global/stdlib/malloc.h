83b892533812d470644f2f803639848d5504a475
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-05-27 11:31:42 -0300

    I forgor

--- Global/stdlib/malloc.h
+++ Global/stdlib/malloc.h
@@ -22,7 +22,7 @@ void *malloc_m(size_t_m size);
 void *realloc_m(void *ptr, size_t_m size);
 void free_m(void *ptr);
 
-void print_m(char avoid_empty);
-void print_m_data();
+// void print_m(char avoid_empty);
+// void print_m_data();
 
 #endif

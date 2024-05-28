795bc9c4cb6b7270ca0b8796a2a33b0e9e308726
Tobias-Noceda <tnoceda@itba.edu.ar>
2024-05-28 12:11:18 -0300

    Update read.c

--- Kernel/files/read.c
+++ Kernel/files/read.c
@@ -11,8 +11,7 @@ size_t read(int fd, void *buffer, size_t count)
 {
     int file = get_current_process()->files[fd];
 
-    bool block = would_block(file);
-    if (block)
+    while (would_block(file))
     {
         read_block(file);
     }

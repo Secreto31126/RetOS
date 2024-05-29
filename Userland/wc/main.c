a25d5440d6701578076fed77d2584c702f624138
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-05-29 20:53:14 -0300

    actually programming wc

--- Userland/wc/main.c
+++ Userland/wc/main.c
@@ -12,10 +12,16 @@ int main(int argc, char *argv[])
 {
 	char str[BLOCK] = {0};
 	int n;
+	char count = 0;
 	while ((n = read(0, str, BLOCK)) > 0)
 	{
-		write(1, str, n);
+		for (int i = 0; i < n; i++)
+		{
+			if (str[i] == '\n')
+				count++;
+		}
 	}
+	write(1, &count, 1);
 	write(1, 0, 1);
 	return 0; // Exit handled outside
 }

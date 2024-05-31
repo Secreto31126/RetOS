d357979110df2bd4baadcdabde2b919d4eafce8a
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-05-31 20:24:06 -0300

    ah, it's because I did fix it before... in only one module

--- Userland/wc/main.c
+++ Userland/wc/main.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
 	}
 	itoa(count, str, 10);
 	write(STD_OUT, str, strlen(str));
-	write(STD_OUT, 0, 1);
+	char c = 0;
+	write(STD_OUT, &c, 1);
 	return 0; // Exit handled outside
 }

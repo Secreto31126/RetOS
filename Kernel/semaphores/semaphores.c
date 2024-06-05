4f50ec18199bf90479ddfe85bafb3dedefb6aebc
Tobias-Noceda <tnoceda@itba.edu.ar>
2024-06-05 14:07:59 -0300

    Better and fixed sems

--- Kernel/semaphores/semaphores.c
+++ Kernel/semaphores/semaphores.c
@@ -3,28 +3,24 @@
 size_t open_sems = 0;
 Semaphore semaphores[MAX_SEMS] = {};
 
+int exists(const char *name, int *i);
+int usable(const sem_t *sem);
+
 sem_t *sem_open(const char *name, unsigned int value)
 {
-    size_t i = 0;
-    size_t sem_id = -1;
-    while (i < open_sems)
+    if (!name || !strlen(name))
     {
-        if (semaphores[i].sem != NULL)
-        {
-            if (!strncmp(semaphores[i].sem->name, name, sizeof(semaphores[i].sem->name)))
-            {
-                semaphores[i].usages++;
-                return semaphores[i].sem;
-            }
-            i++;
-        }
-        if (sem_id == -1)
-        {
-            sem_id = i;
-        }
+        return NULL;
     }
 
-    if (sem_id == -1 || open_sems == MAX_SEMS)
+    int id = 0;
+    if (exists(name, &id))
+    {
+        semaphores[id].usages++;
+        return semaphores[id].sem;
+    }
+
+    if (open_sems == MAX_SEMS)
     {
         return NULL;
     }
@@ -35,32 +31,91 @@ sem_t *sem_open(const char *name, unsigned int value)
         return NULL;
     }
 
-    strncpy(new_sem->name, name, sizeof(semaphores[i].sem->name));
+    strncpy(new_sem->name, name, sizeof(semaphores[id].sem->name));
     new_sem->value = value;
 
-    semaphores[sem_id].sem = new_sem;
-    semaphores[sem_id].usages = 1;
+    semaphores[id].sem = new_sem;
+    semaphores[id].usages = 1;
     open_sems++;
 
     return new_sem;
 }
 
-int sem_wait(sem_t *sem)
+int sem_close(sem_t *sem)
 {
-    size_t i = 0;
-    while (i < open_sems)
+    if (!sem)
+    {
+        return -1;
+    }
+
+    for (size_t i = 0; i < MAX_SEMS; i++)
     {
         if (semaphores[i].sem != NULL)
         {
             if (semaphores[i].sem == sem)
             {
-                break;
+                free(semaphores[i].sem);
+                semaphores[i].sem = NULL;
+                open_sems--;
+                return 0;
             }
-            i++;
         }
     }
 
-    if (i == open_sems)
+    return -1;
+}
+
+int sem_unlink(const char *name)
+{
+    if (!name || !strlen(name))
+    {
+        return -1;
+    }
+
+    int i = 0;
+    if (exists(name, &i))
+    {
+        semaphores[i].usages--;
+        if (semaphores[i].usages == 0)
+        {
+            free(semaphores[i].sem);
+            semaphores[i].sem = NULL;
+            open_sems--;
+        }
+        return 0;
+    }
+
+    return -1;
+}
+
+int sem_post(sem_t *sem)
+{
+    if (!usable(sem))
+    {
+        return -1;
+    }
+
+    unsigned int value;
+    do
+    {
+        // Loop trying to set the spinlock
+        value = exchange(&sem->value, SPINLOCK);
+    } while (SPINLOCK_LOCKED(value));
+
+    if (value + 1 < SPINLOCK)
+    {
+        exchange(&sem->value, ++value);
+        return 0;
+    }
+
+    return -1;
+}
+
+int sem_wait(sem_t *sem)
+{
+    size_t i = 0;
+
+    if (!usable(sem))
     {
         return -1;
     }
@@ -86,69 +141,38 @@ SPINLOCK_CHECK:
     return 0;
 }
 
-int sem_post(sem_t *sem)
+int exists(const char *name, int *i)
 {
-    unsigned int value;
-    do
+    int free = -1;
+    for (size_t j = 0; j < MAX_SEMS; j++)
     {
-        // Loop trying to set the spinlock
-        value = exchange(&sem->value, SPINLOCK);
-    } while (SPINLOCK_LOCKED(value));
-
-    if (value + 1 < SPINLOCK)
-    {
-        exchange(&sem->value, ++value);
-        return 0;
-    }
-
-    return -1;
-}
-
-int sem_unlink(const char *name)
-{
-    if (!name || !strlen(name))
-    {
-        return -1;
-    }
-
-    for (size_t i = 0; i < open_sems;)
-    {
-        if (semaphores[i].sem != NULL)
+        if (semaphores[j].sem != NULL)
         {
-            if (strncmp(semaphores[i].sem->name, name, sizeof(semaphores[i].sem->name)) == 0)
+            if (strncmp(semaphores[j].sem->name, name, strlen(name)) == 0)
             {
-                semaphores[i].usages--;
-                if (!semaphores[i].usages)
-                {
-                    sem_close(semaphores[i].sem);
-                    semaphores[i].sem = NULL;
-                    open_sems--;
-                }
-                return 0;
+                *i = j;
+                return 1;
             }
-            i++;
+        }
+        else if (free == -1)
+        {
+            free = j;
         }
     }
 
-    return -1;
+    *i = free;
+    return 0;
 }
 
-int sem_close(sem_t *sem)
+int usable(const sem_t *sem)
 {
-    for (size_t i = 0; i < open_sems;)
+    for (size_t i = 0; i < MAX_SEMS; i++)
     {
-        if (semaphores[i].sem != NULL)
+        if (semaphores[i].sem == sem)
         {
-            if (semaphores[i].sem == sem)
-            {
-                free(semaphores[i].sem);
-                semaphores[i].sem = NULL;
-                open_sems--;
-                return 0;
-            }
-            i++;
+            return 1;
         }
     }
 
-    return -1;
+    return 0;
 }

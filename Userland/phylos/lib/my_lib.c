52efbb34fb4cf3a22713ee9798e9327582a51f41
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-09 14:09:09 -0300

    Update my_lib.c

--- Userland/phylos/lib/my_lib.c
+++ Userland/phylos/lib/my_lib.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys.h>
 
+// This function lets you create a new (consistent) name for a sem without appending a number to the original string, which would require a buffer or memory allocation
 void rot_n(char *s, unsigned int num)
 {
     while (*s)

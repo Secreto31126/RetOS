e3f911662b62bc800182c93da168c304db143045
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-10 01:11:10 -0300

    D:
    documentation

--- Userland/SampleCodeModule/shell/commandHandler/commandHandlerPrivate.h
+++ Userland/SampleCodeModule/shell/commandHandler/commandHandlerPrivate.h
@@ -3,26 +3,29 @@
 #define COMMAND_COUNT 7
 #include <stdint.h>
 #include <stdlib.h>
+
 /**
- * Currently:
- * set highlight and letter colors .
- * warp up
- * change letter size .
- * change command prompt
- * play snake .
- * snake themes .
- * sing
- * exit
- * help .
+ * @brief Any module wrapper must adjust to this definition
+ *
  */
 typedef moduleData (*action_t)(moduleData, displayStyles *);
+/**
+ * @brief For referencing a module's wrapper
+ *
+ */
 typedef struct command
 {
     char *code;
     char *help;
     action_t action;
 } command;
-
+/**
+ * @brief Adds a command to the list of commands stored in the commandHandler
+ *
+ * @param commandCode The string that identifies the module
+ * @param help A brief explanation of the module's use and function
+ * @param action Function pointer to the module's action_t wrapper
+ */
 void addCommand(char *commandCode, char *help, action_t action);
 
 #endif

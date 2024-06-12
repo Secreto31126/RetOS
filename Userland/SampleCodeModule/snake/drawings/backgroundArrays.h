e992e4a9bf9b5629153773c64aced30580302938
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-12 15:39:50 -0300

    A sad day for RetOS
    It fixes the thing though

--- Userland/SampleCodeModule/snake/drawings/backgroundArrays.h
+++ Userland/SampleCodeModule/snake/drawings/backgroundArrays.h
@@ -8,12 +8,8 @@ extern HexColor windowsColorMap[];
 extern char windowsArray[BACKGROUND_HEIGHT][BACKGROUND_WIDTH];
 extern HexColor marioColorMap[];
 extern char marioArray[BACKGROUND_HEIGHT][BACKGROUND_WIDTH];
-extern HexColor pongColorMap[];
-extern char pongArray[BACKGROUND_HEIGHT][BACKGROUND_WIDTH];
 extern HexColor creationColorMap[];
 extern char creationArray[BACKGROUND_HEIGHT][BACKGROUND_WIDTH];
-extern HexColor camelotColorMap[];
-extern char camelotArray[BACKGROUND_HEIGHT][BACKGROUND_WIDTH];
 extern HexColor idyllicColorMap[];
 extern char idyllicArray[BACKGROUND_HEIGHT][BACKGROUND_WIDTH];
 

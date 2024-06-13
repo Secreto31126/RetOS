966e3508eda2285e67421790f70f769b96fc9c93
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-13 04:34:28 -0300

    Update scheduler.c

--- Kernel/proc/scheduler.c
+++ Kernel/proc/scheduler.c
@@ -115,7 +115,7 @@ void add_il(iterableList il, pid_t pid)
     pNode *to_add = malloc(sizeof(pNode));
     to_add->next = il->first;
     to_add->pid = pid;
-    
+
     if (!il->size || il->current == NULL)
         il->current = to_add;
     il->first = to_add;
@@ -142,7 +142,7 @@ pid_t remove_il_rec(pNode *p, pid_t pid)
 static Process *last = NULL;
 static const char weights[PRIO_MAX - PRIO_MIN] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
 static char schedule[PRIO_MAX - PRIO_MIN] = {0};
-static iterableList *processes[PRIO_MAX - PRIO_MIN] = {0};
+static iterableList processes[PRIO_MAX - PRIO_MIN] = {0};
 
 void setSchedule()
 {

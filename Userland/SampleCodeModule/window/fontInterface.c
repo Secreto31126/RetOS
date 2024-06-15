a85b2b9bfb128979aa45dd75acaffc4047bbcec2
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2024-06-15 02:54:50 -0300

    let's call it a wip

--- Userland/SampleCodeModule/window/fontInterface.c
+++ Userland/SampleCodeModule/window/fontInterface.c
@@ -31,34 +31,3 @@ void drawCharToWindow(Window w, char c, HexColor letterColor, HexColor highlight
 {
     toHexArray((currentFont->letters) + ((int)c % ASCII_MAX) * TRUE_LETTER_HEIGHT * TRUE_LETTER_WIDTH, w.pixels, TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, w.width, w.height, 2, highlightColor, letterColor); // This is equivalent and quicker
 }
-
-/**
- * @deprecated drawCharToWindow has more finesse (and can carry a paint bucket)
- */
-char drawStringToWindow(Window w, char *string, HexColor letterColor, HexColor highlightColor, double size)
-{
-    int index = 0;
-    double width = TRUE_LETTER_WIDTH * size;
-    double height = TRUE_LETTER_HEIGHT * size;
-    uint64_t drawLimitX = w.width - width;
-    uint64_t drawLimitY = w.height - height;
-    for (double i = 0; i < drawLimitY; i += height) // Whole window must be colored, as the window is not ensured to be empty (Otherwise, backspace and similar would not affect window)
-        for (double j = 0; j < drawLimitX; j += width)
-        {
-            if (string[index])
-            {
-                char *letter;
-                letter = (currentFont->letters) + ((int)string[index] % ASCII_MAX) * TRUE_LETTER_HEIGHT * TRUE_LETTER_WIDTH; // Surprisingly enough, this is correct
-                HexColor aux[TRUE_LETTER_HEIGHT][TRUE_LETTER_WIDTH];
-                drawFromHexArray(w, toHexArray(letter, (HexColor *)aux, TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, 2, highlightColor, letterColor), TRUE_LETTER_WIDTH, TRUE_LETTER_HEIGHT, (int)(j + 0.5), (int)(i + 0.5), size, size); // Arte.
-                index++;
-            }
-            else
-            {
-                // If we had bothered to finish this function, it would fill the rest of the window in black. We did not. We also did not erase it, it is a decent reference.
-            }
-        }
-    if (string[index])
-        return 0;
-    return 1;
-}
--- Userland/SampleCodeModule/window/painter.c
+++ Userland/SampleCodeModule/window/painter.c
@@ -5,6 +5,7 @@
 static double size = 1.0;
 static uint64_t width, height, xPointer = 0, yPointer = TRUE_LETTER_HEIGHT;
 static Window stamp;
+static char justBlanked = 0;
 
 void startPainter(uint64_t w, uint64_t h)
 {
@@ -50,13 +51,64 @@ void paintBackSpace()
     xPointer -= TRUE_LETTER_WIDTH * size;
     drawWindow(stamp, xPointer, yPointer);
 }
+void drawCharOnWindow(Window w, char c, HexColor letterColor, HexColor highlightColor, uint64_t x, uint64_t y);
+void paintBackSpaceOnWindow(Window w)
+{
+    if (xPointer <= 0)
+    {
+        yPointer -= size * TRUE_LETTER_HEIGHT;
+        xPointer = X_LINE_END;
+    }
+    xPointer -= TRUE_LETTER_WIDTH * size;
+    drawCharOnWindow(w, 0, 0xFF000000, 0xFF000000, xPointer, yPointer);
+}
 void drawCharAt(char c, HexColor letterColor, HexColor highlightColor, uint64_t x, uint64_t y)
 {
     drawCharToWindow(stamp, c, letterColor, highlightColor);
     drawWindow(stamp, x, y);
 }
+void drawCharOnWindow(Window w, char c, HexColor letterColor, HexColor highlightColor, uint64_t x, uint64_t y)
+{
+    drawCharToWindow(stamp, c, letterColor, highlightColor);
+    drawFromHexArray(w, stamp.pixels, stamp.width, stamp.height, x, y, 1, 1);
+}
+char paintCharOnWindow(Window w, char c, HexColor letterColor, HexColor highlightColor)
+{
+    if (c == '\b')
+    {
+        if (!(xPointer <= 0 && yPointer <= 0)) // shouldn't be able to backspace through line starters, but shell should handle that.
+        {
+            paintBackSpaceOnWindow(w);
+        }
+        return 1;
+    }
+    if ((xPointer + TRUE_LETTER_WIDTH * size) > width || c == '\n')
+    {
+        if ((yPointer + TRUE_LETTER_HEIGHT * size * 2) > height) // *2 because letters are drawn from cursor downwards and to the right, so otherwise last line would have its top on the bottom of the screen
+            return 0;
+        else
+        {
+            newLine();
+            if (c == '\n')
+                return 1;
+        }
+    }
+
+    if (c == '\t')
+    {
+        for (int i = 0; i < SPACES_IN_TAB; i++)
+            paintCharOnWindow(w, ' ', letterColor, highlightColor);
+    }
+    else
+    {
+        drawCharOnWindow(w, c, letterColor, highlightColor, xPointer, yPointer);
+        moveCursor();
+    }
+    return 1;
+}
 char paintChar(char c, HexColor letterColor, HexColor highlightColor)
 {
+    justBlanked = 0;
     if (c == '\b')
     {
         if (!(xPointer <= 0 && yPointer <= 0)) // shouldn't be able to backspace through line starters, but shell should handle that.
@@ -89,8 +141,33 @@ char paintChar(char c, HexColor letterColor, HexColor highlightColor)
     }
     return 1;
 }
+char fastPaintString(const char *c, HexColor letterColor, HexColor highlightColor)
+{
+    if (!justBlanked)
+        return 2;
+    uint64_t h = getScreenHeight(), w = getScreenWidth();
+    Window wholeScreen = getWindow(w, h, pshm(h * w * sizeof(HexColor))); // uses pshm, as the image does not fit in ~128 kb for memory assignation
+    if (wholeScreen.pixels == NULL)
+        return 2;
+    justBlanked = 0;
+
+    while (*c && paintCharOnWindow(wholeScreen, *c, letterColor, highlightColor))
+    {
+        c++;
+    }
+    quickDraw(wholeScreen);
+    free_shm(wholeScreen.pixels);
+    if (*c)
+        return 0;
+    return 1;
+}
 char paintString(const char *c, HexColor letterColor, HexColor highlightColor)
 {
+    char aux = fastPaintString(c, letterColor, highlightColor);
+    if (!aux)
+        return 0;
+    if (aux == 1)
+        return 1;
     while (*c && paintChar(*c, letterColor, highlightColor))
     {
         c++;
@@ -147,6 +224,7 @@ void blank()
 {
     xPointer = 0;
     yPointer = TRUE_LETTER_HEIGHT * size;
+    justBlanked = 1;
     clear();
 }
 void endPainter()

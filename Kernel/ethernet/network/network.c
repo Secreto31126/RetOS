96ae091a10b848a1515b947eedd063ff14fe13a5
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-07-30 18:33:59 -0300

    Less verbose

--- Kernel/ethernet/network/network.c
+++ Kernel/ethernet/network/network.c
@@ -53,11 +53,11 @@ int transmit(uint8_t *dest, const void *data, int length, uint16_t protocol)
     frame->type = endian_word(protocol);
     memcpy(frame_data, data, length);
 
-    ncPrint("Transmitting packet to ");
-    char mac_str[20];
-    snprintf(mac_str, 20, "%02x:%02x:%02x:%02x:%02x:%02x", frame->dest[0], frame->dest[1], frame->dest[2], frame->dest[3], frame->dest[4], frame->dest[5]);
-    ncPrint(mac_str);
-    ncNewline();
+    // ncPrint("Transmitting packet to ");
+    // char mac_str[20];
+    // snprintf(mac_str, 20, "%02x:%02x:%02x:%02x:%02x:%02x", frame->dest[0], frame->dest[1], frame->dest[2], frame->dest[3], frame->dest[4], frame->dest[5]);
+    // ncPrint(mac_str);
+    // ncNewline();
 
     int sent = rtl8139_send_packet(frame, sizeof(EthernetFrame) + length);
 
